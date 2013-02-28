a0f60e5b67415d7707ab148dfe35a0f7ef1b2e56
joubin <joubin.j@gmail.com>
2013-02-28 15:48:48 -0800

    Icon animation, backend integration, user behavior for testing

--- background.js
+++ background.js
@@ -1,19 +1,48 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-if( localStorage["mainLogin"] == null){
+	if( localStorage["id"] == null){
+		var asd = new XMLHttpRequest();
+		asd.onreadystatechange = function() {
+		    if (asd.readyState == 4) {
+		        localStorage["id"] = asd.responseText;
+		    }
+		}
+		asd.open('GET', 'http://example.com/uploads3/test.php', true); // Get a uniqID for every extension on each computer
+		asd.send(null);	
+			
+	};
+	if( localStorage["mainLogin"] == null){
 	alert("Please go to the option page to login");
-}
- chrome.tabs.captureVisibleTab(null, function(img) {
-	chrome.browserAction.setBadgeText ( { text: "working..." } );
+	chrome.tabs.create({'url': chrome.extension.getURL('option.html')}, function(tab) {
+	  // Tab opened.
+	});
+	return false; //stops the rest of the code
+	}
+ 	chrome.tabs.captureVisibleTab(null, function(img) {
+	chrome.browserAction.setBadgeText ( { text: "Uploading" } );
 	setTimeout(function () {
 	    chrome.browserAction.setBadgeText( { text: "" } );
 	}, 1000);
-    var xhr = new XMLHttpRequest(), formData = new FormData();  
+	//send the data, this works just like like a html form works. 
+	var xhr = new XMLHttpRequest(), formData = new FormData();  
     formData.append("user", localStorage["mainLogin"]);
+    formData.append("userID", localStorage["id"]);
  	formData.append("img", img);
-	alert(localStorage["mainLogin"]);
-	alert(img);
+ 	formData.append("url", tab.url);
+	//alert(localStorage["mainLogin"]);
+	//alert(img);
+	//Get image stuff
+	xhr.onreadystatechange = function (event) {
+	    if (xhr.readyState === 4 && xhr.status === 200) {
+	       var theResponse = xhr.responseText;
+		//	alert("PictureID is "+ theResponse + " userID=" + localStorage["id"]);
+		//alert(theResponse);
+		if(theResponse != "Stop Cheating! This has been reported.")
+			chrome.windows.create({'url': "http://example.com//prototype2.php?pictureID="+ theResponse + "&userID=" + localStorage["id"]+"&username="+  localStorage["mainLogin"]}, function(tab) {
+			  // open window
+			});
+	}};	
 
-    xhr.open("POST", "UploadSite", true);
+    xhr.open("POST", "http://example.com//upload_file.php", true);
     xhr.send(formData);
 	
   });
--- option.js
+++ option.js
@@ -1,34 +1,42 @@
 function saveChanges() {
-  var theCity = city.value;
-  var theState = state.value;
-  var theEmail = email.value;
-  var theAge = age.value;
-  var theGender = gender.value;
-  var theOther = other.value;
-	
+  var theUser = user.value;
+  var thePass = pass.value;
+  var realID = localStorage["id"];
+  localStorage["mainLogin"] = theUser;
 
    var xhr = new XMLHttpRequest(), formData = new FormData();  
-    formData.append("city", theCity);
-    formData.append("state", theState);
-    formData.append("email", theEmail);
-    formData.append("age", theAge);
-    formData.append("gender", theGender);
-    formData.append("other", theOther);
+    formData.append("user", theUser);
+    formData.append("pass", thePass);
+    formData.append("realID", realID);
+
 
 	xhr.onreadystatechange = function (event) {
 	    if (xhr.readyState === 4 && xhr.status === 200) {
-	       var theValue = xhr.responseText; // value should equal "1234"
-			localStorage["mainLogin"] = theValue;
+	       var theResponse = xhr.responseText; 
+	
+				if( theResponse == 1){
+					localStorage["mainLogin"] = theUser;
+					alert("All good, you can begin");
+					chrome.tabs.getCurrent(function(tab) {
+					    chrome.tabs.remove(tab.id, function() { });
+					});
+				};
+				if(theResponse == 0){
+					alert("Wrong user or passowrd");
+				};
+				if(theResponse != 0 && theResponse != 1){
+					alert("getting id");
+					localStorage["mainLogin"] = theUser;
+					localStorage["id"] = theResponse;
+					chrome.tabs.getCurrent(function(tab) {
+					    chrome.tabs.remove(tab.id, function() { });
+					});
+				};	
 	    }
 	};
-    xhr.open("POST", "UploadSite", true);
+    xhr.open("POST", "http://example.com//Signups/check.php", true);
     xhr.send(formData);
-	alert("Okay, now the extension is ready to be used.");
 
-	chrome.tabs.getCurrent(function(tab) {
-	    chrome.tabs.remove(tab.id, function() { });
-	});
-	
 }
 
 window.onload=function (){
